88102798fecc62c62e28e7408caf2e0ea021d606
전해지 <wjsgowl0224@naver.com>
2025-11-02 22:34:41 +0900

    Feat: OpenAI 임베딩 모델 구현체 Initializer 및 add_documents 메서드 개발

--- retriever/embedding_model/base_embedding_model.py
+++ retriever/embedding_model/base_embedding_model.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
 
+
 class BaseEmbeddingModel(ABC):
     @abstractmethod
-    def embed_documents(self, documents: list[str], *args):
+    def embed_documents(self, documents: list[str], *args) -> list[list[float]]:
         pass
 
     @abstractmethod
-    def embed_query(self, information_need: str, *args):
+    def embed_query(self, information_need: str, *args) -> list[float]:
         pass
--- retriever/embedding_model/openai_embedding_model.py
+++ retriever/embedding_model/openai_embedding_model.py
@@ -1,12 +1,29 @@
+import traceback
 from langchain_openai import OpenAIEmbeddings
 from embedding_model.base_embedding_model import BaseEmbeddingModel
 
 
 class OpenAIEmbeddingModel(BaseEmbeddingModel):
-    embed = OpenAIEmbeddings(
-        model="text-embedding-3-large"
-        # With the `text-embedding-3` class
-        # of models, you can specify the size
-        # of the embeddings you want returned.
-        # dimensions=1024
-    )
+
+    def __init__(self, model: str, api_key: str) -> None:
+        self.embedding_model = OpenAIEmbeddings(
+            model="text-embedding-3-large",
+            dimensions=1024
+        )
+
+    def embed_documents(self, documents: list[str]) -> list[list[float]]:
+
+        if not documents:
+            return []
+
+        try:
+            document_vectors = self.embedding_model.embed_documents(documents)
+        except Exception as e:
+            error_details = traceback.format_exc()
+            raise RuntimeError(
+                f"❌ Embedding failed for documents batch:\n{error_details}"
+            ) from e
+
+        return document_vectors
+
+    # def embed_query(self, information_need: str, *args):
