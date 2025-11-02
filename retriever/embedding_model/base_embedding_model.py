cf2b7bb81836a365e9b7b0a22b88d0f07b5939b6
전해지 <wjsgowl0224@naver.com>
2025-11-02 21:50:19 +0900

    Feat: 임베딩 모델 추상 클래스 개발

--- retriever/embedding_model/base_embedding_model.py
+++ retriever/embedding_model/base_embedding_model.py
@@ -1,4 +1,11 @@
 from abc import ABC, abstractmethod
 
 class BaseEmbeddingModel(ABC):
-    pass
+
+    @abstractmethod
+    def embed_documents(self, documents: list[str], *args):
+        pass
+
+    @abstractmethod
+    def embed_query(self, information_need: str, *args):
+        pass
