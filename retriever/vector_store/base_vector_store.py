a483e4a5eeee45a8f4be2342a0cea2bd14f871a5
전해지 <wjsgowl0224@naver.com>
2025-11-03 09:32:39 +0900

    Fix: 벡터스토어 추상 클래스 매개변수 잘못 선언돼있던 거 정정

--- retriever/vector_store/base_vector_store.py
+++ retriever/vector_store/base_vector_store.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from langchain_core.documents import Document
+from retriever.embedding_model.base_embedding_model import BaseEmbeddingModel
 
 
 class BaseVectorStore(ABC):
     @abstractmethod
-    def add_documents(self, documents: list[Document], embedding_model, *args):
+    def add_documents(self, documents: list[Document], embedding_model: BaseEmbeddingModel, *args):
         pass
 
     @abstractmethod
-    def similarity_search(self, query_text: str, top_k: int = 5, *args):
+    def similarity_search(self, information_need: str, embedding_model: BaseEmbeddingModel, *args):
         pass
